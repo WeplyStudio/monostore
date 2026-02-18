9c6ba7076a20f1c4db0dae61aefac570a6fa62ae
Jaysss <matchboxdevelopment@gmail.com>
2026-02-18 06:05:51 +0000

    Try fixing this error: `Console Error: Login Error: "auth/api-key-not-va

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "PLACEHOLDER",
-  authDomain: "PLACEHOLDER",
-  projectId: "PLACEHOLDER",
-  storageBucket: "PLACEHOLDER",
-  messagingSenderId: "PLACEHOLDER",
-  appId: "PLACEHOLDER"
+  apiKey: "AIzaSyDyjovfUhAhXAozUs3ITD6ribaedGb2AHc",
+  authDomain: "studio-1112206349-ab113.firebaseapp.com",
+  projectId: "studio-1112206349-ab113",
+  storageBucket: "studio-1112206349-ab113.firebasestorage.app",
+  messagingSenderId: "388928927556",
+  appId: "1:388928927556:web:1dabcbdfceef8c59954911"
 };
